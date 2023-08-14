80f4202c15ba47c07ba26bb04f5439b3474cef7b
Nirushetty10 <nirushetty582@gmail.com>
2023-08-14 15:57:21 +0530

    logo added

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import classes from "./NavBar.module.scss";
+import Logo from "../../Assets/Logo/logo.png";
 
 const NavBar = () => {
   return (
     <div className={classes.navBar}>
         <div className={classes.navContainer}>
-            <h2>KudlaStore</h2>
+            <img src={Logo} alt="" />
             <input type="text" name="" id="" />
             <button>Login</button>
         </div>
